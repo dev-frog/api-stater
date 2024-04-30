fbc3be3ec8bf207057282406197fa2fdb58f7588
dev-frog <kali.nisga@gmail.com>
2024-05-01 01:51:15 +0600

    Add routes for API endpoints

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,15 @@ import helmet from 'helmet'
 import cors, { CorsOptions } from 'cors'
 import 'dotenv/config'
 
+import router from './routes'
+
 const app: Express = express()
 
 app.use(express.json())
 app.use(helmet())
 
 const corsOptions: CorsOptions = {
-  origin: 'http://localhost:8080'
+  origin: ['*', 'http://localhost:3000', 'https://localhost:3000', 'http://localhost:300']
 }
 
 app.use(cors(corsOptions))
@@ -18,5 +20,7 @@ app.use(cors(corsOptions))
 app.get('/', (req: Request, res: Response) => {
   res.status(status.OK).json({ message: 'Hello World', success: true })
 })
+
+app.use('/api/v1', router)
 
 export default app
